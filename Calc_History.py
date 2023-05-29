c5d3524a1943e240360f2949ae2ede750b438bb1
GitHubBot0111 <rishikeshpandeypresidium@gmail.com>
2023-05-29 21:57:26 +0530

    copied original file

--- Calc_History.py
+++ Calc_History.py
@@ -1,2 +1,91 @@
-Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+# This function adds two numbers
+import Addition
+import Subtraction
+import Multiplication
+import Division
+import power
+import square
+import sqrt
+import factorial
+
+
+# This is to present a menu to the user
+print("Select operation.")
+print("1.Addition")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+print("5.Power")
+print("6.Square")
+print("7.Square root")
+print("8.Factorial")
+
+lst=[]
+while True:
+    # take input from the user
+    choice = input("Enter choice(1/2/3/4/5/6/7/8): ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if choice == '1':
+            print(num1, "+", num2, "=", Addition.add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", Subtraction.subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", Multiplication.multiply(num1, num2))
+
+        elif choice == '4':
+            try:
+                print(num1, "/", num2, "=", Division.divide(num1, num2))
+            except ZeroDivisionError:
+                print("ZeroDivisionError! Please ensure second number is not 0")
+        
+        # check if user wants another calculation
+        # break the while loop if answer is no
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+      
+    elif choice in ('6','7','8'):
+        try:
+            num=float(input("Enter number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+        if choice == '6':
+            print("Square of", num, "=", square.square(num))
+        elif choice == '7':
+            print("Square root of", num, "=", sqrt.sqrt(num))
+        elif choice == '8':
+            print("Factorial of", num, "=", factorial.fact(num))
+            
+        # check if user wants another calculation
+        # break the while loop if answer is no
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+      
+    elif choice == "5":
+        try:
+            num=float(input("Enter base: "))
+            pow=float(input("Enter power: "))
+            print(num, "raised to the power", pow, "=", power.power(num,pow))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    else:
+        print("Invalid Input")
+
+
